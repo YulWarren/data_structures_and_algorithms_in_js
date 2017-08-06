a603f28f31001bdde7f19b5f2ea7c8856879b552
yulwarren <wangyuyang7781@gmail.com>
2017-08-06 11:26:33 +0800

    add shell sort and merge sort, but i dont understand the merge sort code

--- 11.sort.js
+++ 11.sort.js
@@ -8,14 +8,22 @@ function CArray(numElements) {
     this.clear = clear;
     this.setData = setData;
     this.swap = swap;
+    this.gaps = [5,3,1]; // shell-sort
+    this.setGap = setGap; // shell-sort
     this.bubbleSort = bubbleSort;
     this.selectionSort = selectionSort;
     this.insertionSort = insertionSort;
+    this.shellSort = shellSort;
+    this.mergeArray = mergeArray;
+    this.mergeSort = mergeSort;
     for (var i = 0; i < numElements; i++) {
         this.dataStore[i] = i;
     }
 }
-
+// only shell sort use this function
+function setGap(arr) {
+    this.gaps = arr;
+}
 function setData() {
     for (var i = 0; i < this.numElements; i++) {
         this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
@@ -114,7 +122,7 @@ function insertionSort() {
     }
 }
 
-var numElements = 100000;
+var numElements = 10;
 var mynums1 = new CArray(numElements);
 mynums1.setData();
 var start1 = new Date().getTime();
@@ -139,3 +147,124 @@ console.log(`对${numElements} 个元素执行插入排序消耗的时间为： 
 
 // 从三种冒泡排序耗时截图中可以看出插入排序最快，冒泡排序最慢
 // 图见timeConsuming.png
+
+// high-level sorting
+// inculding Shell sorting, merge sorting, quick sorting
+// 希尔排序在插入排序的基础上做了改进。会首先比较距离较远的元素，而非相邻元素。可以使离正确位置很远的元素更快地回到合适的位置。
+// 当开始用这个算法遍历数据集时，所有元素直接的距离会不断减小，直到处理到数据集的末尾，这时算法比较的就是相邻元素了。
+// 希尔排序的工作原理是：通过定义一个间隔序列来表示在排序过程中进行比较的元素之间有多远的间隔。可以动态定义此间隔。
+function shellSort() {
+    for (var g = 0; g < this.gaps.length; g++){ // 对间隔序列遍历
+        for (var i = this.gaps[g]; i < this.dataStore.length; i++) { // 对待排序数组进行遍历，从第g个元素开始
+            var temp = this.dataStore[i]; // 暂存数据
+            for (var j = i; j >= this.gaps[g] && this.dataStore[j - this.gaps[g]] > temp; j -= this.gaps[g]) { // 对已排序数组进行遍历，判断与已选待排元素谁大，每次将j减小g个
+                this.dataStore[j] = this.dataStore[j - this.gaps[g]]; //如果已排序大于待排序，则已排序占据待排序位置
+            }
+            this.dataStore[j] = temp; // 将暂存数据（原待排序数据）存入已排序数据空出来的位置
+        }
+    }
+}
+// 动态计算间隔序列的希尔排序
+function shellSortDyn() {
+    var N = this.dataStore.length;
+    var h = 1;
+    while (h < N / 3) {
+        h = 3 * h + 1;
+    }
+    while (h >= 1) {
+        for (var i = h; i < N; i++) {
+            for (var j = i; j >= h && this.dataStore[j] < this.dataStore[j - h]; j -= h) {
+                swap(this.dataStore, j, j - h);
+            }
+        }
+        h = (h - 1) / 3;
+    }
+}
+
+var nums = new CArray(10);
+nums.setData();
+console.log('before shell sort: \n');
+var start = new Date().getTime();
+console.log(nums.toString());
+console.log('\n on shell sorting: \n');
+nums.shellSort();
+var stop = new Date().getTime();
+console.log('\n after shell sorting:\n');
+console.log(nums.toString())
+console.log(`time consume: ${stop-start}`)
+
+
+
+// merge sort
+// 原理：把一系列排好序的子序列合并成一个大的完整有序序列
+// 需要两个排好序的子数组，通过比较数据大小，先从最小的数据开始插入，最后合并得到第三个数组
+// 1、自顶向下的归并排序，需要递归，但对js来说这种算法的递归深度太深
+// 2、自底向上的归并排序
+// 首先将数据集分解为一组只有一个元素的数组，然后通过创建一组左右子数组将它们慢慢合并起来，每次合并都保存一部分排好序的数据，直到最后剩下的这个数组所有的数据都已排序。
+// 原理懂了，但是为什么这样写？？？？？？
+function mergeSort() {
+    // 如果数据少于2个，没有排序的必要
+    if (this.dataStore.length < 2) {
+        return;
+    }
+    // step控制left和right子序列的大小
+    var step = 1;
+    var left, right;
+    // 只要子序列小于数组长度，就一直循环
+    while (step < this.dataStore.length) {
+        // 左序列初始化为0，右序列初始化为子序列大小
+        left = 0;
+        right = step;
+        
+        while(right + step <= this.dataStore.length) {
+            mergeArray(this.dataStore, left, left + step, right, right + step);
+            left = right + step;
+            right = left + step;
+        }
+        if (right < this.dataStore.length) {
+            mergeArray(this.dataStore, left, left + step, right, this.dataStore.length);
+        }
+        step *= 2;
+        console.log(this.toString())
+    }
+}
+// 合并数组
+function mergeArray(arr, startLeft,stopLeft,startRight,stopRight) {
+    //创建左右数组
+    var rightArr = new Array(stopRight - startRight + 1);
+    var leftArr = new Array(stopLeft - startLeft + 1);
+    // 将传入的arr数据传入刚创建的数组
+    var k = startRight;
+    for (var i = 0; i < (rightArr.length - 1); i++) {
+        rightArr[i] = arr[k];
+        k++
+    }
+    k = startLeft;
+    for (var i = 0; i < (leftArr.length - 1); i++) {
+        leftArr[i] = arr[k];
+        k++;
+    }
+    // 设置最后一个数据为哨兵值
+    rightArr[rightArr.length - 1] = Infinity;
+    leftArr[leftArr.length - 1] = Infinity;
+    // 如果左数组的值大于右数组的值，就将左数组的值放到传入的arr中
+    var m = 0;
+    var n = 0;
+    for (var k = startLeft; k < stopRight; k++) {
+        if (leftArr[m] <= rightArr[n]) {
+            arr[k] = leftArr[m];
+            m++
+        } else {
+            arr[k] = rightArr[n];
+            n++;
+        }
+    }
+}
+
+var nums2 = new CArray(10);
+nums2.setData();
+console.log('before merge:\n')
+console.log(nums2.toString());
+nums2.mergeSort();
+console.log('\nafter merge\n')
+console.log(nums2.toString())
